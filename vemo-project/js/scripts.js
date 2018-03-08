d7ad141f59237fd281f2898c279a8a8cdeb99a2b
Omar <omar_zikry@github.com>
2018-03-08 15:30:51 +0200

    bug fixes

--- vemo-project/js/scripts.js
+++ vemo-project/js/scripts.js
@@ -53,15 +53,3 @@ $('a[href*="#"]')
       }
     }
   });
-
-//preloader
-  $(document).ready(function () {
-    //Preloader
-    $(window).load(function () {
-      preloaderFadeOutTime = 500;
-      function hidePreloader() {
-        var preloader = $('.spinner-wrapper');
-        preloader.fadeOut(preloaderFadeOutTime);
-      }
-      hidePreloader();
-    })});
