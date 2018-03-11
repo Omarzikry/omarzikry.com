f85ca42cea9786dd2e28ca574af61499742f10ff
Omar <omar_zikry@github.com>
2018-03-11 03:55:01 +0200

    preloader added

--- vemo-project/js/scripts.js
+++ vemo-project/js/scripts.js
@@ -1,3 +1,22 @@
+window.onload = init();
+function init(){
+  var header = document.getElementById('header');
+  var main = document.getElementById('main');
+  var footer = document.getElementById('footer');
+  var body = document.getElementById('body');
+  var preloader = document.getElementById('preloader');
+  var preloader2 = document.getElementById('preloader2');
+  var preloader3 = document.getElementById('preloader3');
+  header.classList.remove('remove');
+  main.classList.remove('remove');
+  footer.classList.remove('remove');
+  body.style.overflow = "visible";
+  body.style.overflowX = "hidden";
+  preloader.style.display = "none";
+  preloader2.style.display = "none";
+  preloader3.style.display = "none";
+  body.style.backgroundColor = "#fff"
+}
 ////toggle button
 $(document).ready(function () {
   $('.nav-btn').click(function () {
