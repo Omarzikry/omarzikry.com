f0ae47c7271030e2704600872d179cbe0be44209
Omar <omar_zikry@github.com>
2018-04-22 17:12:27 +0200

    updates

--- creative-project/js/scripts.js
+++ creative-project/js/scripts.js
@@ -4,8 +4,49 @@ $(document).ready(function () {
         $('.fullnav').toggleClass('open-nav');
         $('.fullnav a').toggleClass('nav-link-transition')
     });
-    var typed = new Typed('#typed', {
-        stringsElement: '#typed-strings',
-        speed : 20
-    });
+
+    // === 3d panel === //
+
+    var $body = $('body'),
+	$panel = $('.panel'),
+	$pContent = $('.panel__content'),
+	$img = $('.panel__img-col');
+
+function initTilt() {
+	TweenMax.set([$pContent, $img], { transformStyle: "preserve-3d" });
+
+	$body.mousemove(function(e) {
+		var sxPos = e.pageX / $panel.width() * 100 - 100;
+		var syPos = e.pageY / $panel.height() * 100 - 100;
+		TweenMax.to($pContent, 2, {
+			rotationY: 0.05 * sxPos,
+			rotationX: -0.03 * syPos,
+			transformPerspective: 500,
+			transformOrigin: "center center -400",
+			ease: Expo.easeOut
+		});
+		TweenMax.to($img, 2, {
+			rotationY: 0.05 * sxPos,
+			rotationX: -0.03 * syPos,
+			transformPerspective: 500,
+			transformOrigin: "center center -200",
+			ease: Expo.easeOut
+		});
+	});
+};
+
+initTilt();
+
+console.clear();
+
+
+
+
+
+
+
+
+
+
+
 })
